6b267a00bb30d58114e60788e0d32bb310b61ff7
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-02-27 14:26:25 +0100

    feat: search page

---
+++ src/app/(pages)/search/[query]/page.tsx
@@ -0,0 +1,30 @@
+import BlocksWrapper from "@/app/components/BlocksWrapper";
+import ErrorDiv from "@/app/components/ErrorDiv";
+import { PageWrapper } from "@/app/components/PageWrapper";
+import ResultBlock from "@/app/components/ResultBlock";
+import ResultsTopBar from "@/app/components/ResultsTopBar";
+import { TMDB } from "@/app/tmdb/lib";
+
+export default async function SearchPage({ params, searchParams }: { 
+  params: Promise<{ [key: string]: string | string[]}> 
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
+  const { query } = await params;
+  const { p: page } = await searchParams;
+  const result = await TMDB.search(query.toString(), parseInt(page + "") || 1);
+
+  if (result.error !== undefined) {
+    return <ErrorDiv message={result.error} />
+  }
+
+  return (
+    <PageWrapper>
+      <ResultsTopBar totalPages={result.ok.total_pages}  title={`Results for "${decodeURI(query.toString())}"`} />
+      <BlocksWrapper>
+        {result.ok.results.map(item => {
+          return <ResultBlock key={item.id} item={item}/>
+        })} 
+      </BlocksWrapper>
+    </PageWrapper>
+  )
+}
