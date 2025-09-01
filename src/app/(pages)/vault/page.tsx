15e5de2be332a874bed9df45d303d0e84fee0254
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-09-01 11:35:28 +0200

    feat: add vault page

---
+++ src/app/(pages)/vault/page.tsx
@@ -0,0 +1,124 @@
+import ErrorDiv from "@/app/components/ErrorDiv";
+import ResultsTopBar from "@/app/components/result/ResultsTopBar";
+import BlocksWrapper from "@/app/components/wrapper/BlocksWrapper";
+import { PageWrapper } from "@/app/components/wrapper/PageWrapper";
+import { getWizardList } from "@/app/db/lib";
+import {
+  Movie,
+  MovieDetails,
+  Res,
+  Result,
+  TMDB,
+  TvDetails,
+  TvShow,
+} from "@/app/tmdb/lib";
+import PageSelect from "../components/PageSelect";
+import ResultBlock from "@/app/components/result/ResultBlock";
+import { notFound } from "next/navigation";
+
+function getType(type?: string | string[]) {
+  if (type === "tv") return "tv";
+  if (type === "movie") return "movie";
+  else return "all";
+}
+
+function getList(list?: string | string[]) {
+  if (list === "watchlist" || list === "played" || list === "finished")
+    return list;
+  else return "played";
+}
+
+const BUTTONS = (type: string, list: string) => [
+  <PageSelect
+    key={0}
+    label="Type"
+    searchParam="t"
+    defaultValue={type}
+    values={[
+      { name: "All", value: "all" },
+      { name: "Movies", value: "movie" },
+      { name: "TV Shows", value: "tv" },
+    ]}
+  />,
+  <PageSelect
+    key={1}
+    label="List"
+    searchParam="l"
+    defaultValue={list}
+    values={[
+      { name: "Watchlist", value: "watchlist" },
+      { name: "Played", value: "played" },
+      { name: "Finished", value: "finished" },
+    ]}
+  />,
+];
+
+function mediaFromDetails(
+  type: "movie" | "tv",
+  detailsRes: Result<MovieDetails | TvDetails>,
+): Result<Movie | TvShow> {
+  if (Res.isError(detailsRes)) return detailsRes;
+  const details = detailsRes.ok;
+
+  if (type === "movie") {
+    const movieDetails = details as MovieDetails;
+    const movie: Movie = {
+      media_type: "movie",
+      genre_ids: movieDetails.genres.map((g) => g.id),
+      ...movieDetails,
+    };
+    return Res.ok(movie);
+  } else {
+    const tvDetails = details as TvDetails;
+    const tvShow: TvShow = {
+      media_type: "tv",
+      genre_ids: tvDetails.genres.map((g) => g.id),
+      ...tvDetails,
+    };
+    return Res.ok(tvShow);
+  }
+}
+
+export default async function VaultPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: undefined | string | string[] }>;
+}) {
+  const { p: page, t: type, l: list } = await searchParams;
+
+  const pType = getType(type);
+  const pList = getList(list);
+  const pPage = parseInt(page + "") || 1;
+
+  const result = await getWizardList(pList, pType, pPage);
+  if (Res.isError(result)) return notFound();
+
+  const listDetails: Result<Movie | TvShow>[] = [];
+  for (const entry of result.ok.results) {
+    const details =
+      entry.type === "movie"
+        ? await TMDB.getMovieDetails(entry.id)
+        : await TMDB.getTvDetails(entry.id);
+
+    listDetails.push(mediaFromDetails(entry.type, details));
+  }
+
+  return (
+    <PageWrapper>
+      <ResultsTopBar
+        totalPages={result.ok.total_pages}
+        title="Vault"
+        buttons={BUTTONS(pType, pList)}
+      />
+      <BlocksWrapper>
+        {listDetails.map((details, i) =>
+          Res.isError(details) ? (
+            <ErrorDiv key={i} message={details.error} />
+          ) : (
+            <ResultBlock key={details.ok.id} item={details.ok} />
+          ),
+        )}
+      </BlocksWrapper>
+    </PageWrapper>
+  );
+}
