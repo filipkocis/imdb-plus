3b72de4803816c688c24054cd4ba2cd6c21cc368
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-09-04 01:05:52 +0200

    fix: add allow autoplay and enc-media to iframe

--- src/app/components/player/EmbeddedPlayer.tsx
+++ src/app/components/player/EmbeddedPlayer.tsx
@@ -1,36 +1,46 @@
-"use client"
+"use client";
 
 import { PlayerOptions } from "@/app/context/PlayerContext";
 import { useServerList } from "@/app/context/ServersContext";
 
-export default function EmbeddedPlayer({ serverOption, player }: { serverOption: number, player: PlayerOptions }) {
-  const { serverList } = useServerList()
+export default function EmbeddedPlayer({
+  serverOption,
+  player,
+}: {
+  serverOption: number;
+  player: PlayerOptions;
+}) {
+  const { serverList } = useServerList();
 
-  if (player.type === 'youtube') return (
-    <div className="">
-      <iframe 
-        src={`https://www.youtube.com/embed/${player.id}?autoplay=1`}
-        id="ytplayer" 
-        width="100%" 
-        height="100%"
-        allowFullScreen
-      ></iframe>
-    </div>
-  )
+  if (player.type === "youtube")
+    return (
+      <div className="">
+        <iframe
+          src={`https://www.youtube.com/embed/${player.id}?autoplay=1`}
+          id="ytplayer"
+          width="100%"
+          height="100%"
+          allowFullScreen
+          allow="autoplay; encrypted-media"
+        ></iframe>
+      </div>
+    );
 
-  const server = serverList[serverOption]
-  const src = player.type === 'tv' ?
-    server.getTv(player.id, player.season, player.episode) :
-    server.getMovie(player.id)
+  const server = serverList[serverOption];
+  const src =
+    player.type === "tv"
+      ? server.getTv(player.id, player.season, player.episode)
+      : server.getMovie(player.id);
 
   return (
     <div className="">
-      <iframe 
+      <iframe
         src={src}
-        width="100%" 
-        height="100%" 
+        width="100%"
+        height="100%"
         allowFullScreen
+        allow="autoplay; encrypted-media"
       ></iframe>
     </div>
-  )
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ scrollbarWidth: "none" }}>
       <WizardProvider>
         <PlayerProvider>
           <ServerListProvider>
